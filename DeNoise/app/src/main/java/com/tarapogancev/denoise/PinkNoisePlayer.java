277c12fc42c0289a4c157352a2bc0c770bfd9c33
Tara Pogančev <tarapogancev@gmail.com>
2022-08-09 13:54:20 +0200

    feat: initial commit

---
+++ DeNoise/app/src/main/java/com/tarapogancev/denoise/PinkNoisePlayer.java
@@ -0,0 +1,14 @@
+package com.tarapogancev.denoise;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PinkNoisePlayer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pink_noise_player);
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# android-white-noise
+# DeNoise - White Noise Application
 DeNoise - White Noise Application for relaxation, studying or sleeping, made in Android Studio.
+
+
+## Resources
+https://www.flaticon.com/packs/music-224?word=music%20player
+https://unsplash.com/
