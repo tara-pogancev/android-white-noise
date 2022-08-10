7950e4d3f91fdfd8286796fa469ad45cd418a072
Tara Pogančev <tarapogancev@gmail.com>
2022-08-10 19:32:52 +0200

    feat: pink noise activity

--- DeNoise/app/src/main/java/com/tarapogancev/denoise/PinkNoisePlayer.java
+++ DeNoise/app/src/main/java/com/tarapogancev/denoise/PinkNoisePlayer.java
@@ -1,14 +1,28 @@
 package com.tarapogancev.denoise;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class PinkNoisePlayer extends AppCompatActivity {
+
+    CardView backButton, timerButton, whiteNoiseButton, brownNoiseButton;
+    ImageView nextButton, previousButton, playPauseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pink_noise_player);
+
+        backButton = findViewById(R.id.button_back);
+        timerButton = findViewById(R.id.button_timer);
+        whiteNoiseButton = findViewById(R.id.card_white);
+        brownNoiseButton = findViewById(R.id.card_brown);
+        nextButton = findViewById(R.id.button_next);
+        previousButton = findViewById(R.id.button_previous);
+        playPauseButton = findViewById(R.id.button_playPause);
+        
     }
 }
