41270a87173365180da97a0685e54197866f77e6
Tara Pogančev <tarapogancev@gmail.com>
2022-08-11 21:26:16 +0200

    feat: added application icon, logo, fixed shadows

--- DeNoise/app/src/main/java/com/tarapogancev/denoise/MainActivity.java
+++ DeNoise/app/src/main/java/com/tarapogancev/denoise/MainActivity.java
@@ -11,7 +11,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Intent intent = new Intent(this, PinkNoisePlayer.class);
-        startActivity(intent);
     }
 }
--- DeNoise/app/src/main/java/com/tarapogancev/denoise/WhiteNoisePlayer.java
+++ DeNoise/app/src/main/java/com/tarapogancev/denoise/WhiteNoisePlayer.java
@@ -7,6 +7,7 @@ import androidx.core.app.ActivityOptionsCompat;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class WhiteNoisePlayer extends AppCompatActivity {
 
